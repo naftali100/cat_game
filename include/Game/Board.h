b7934b2f28b8096cee61c1e16731d98324e28e48
Hoffman Tzvi <hoffmantz@std.hac.ac.il>
2022-04-25 20:28:28 +0300

    board

--- include/Game/Board.h
+++ include/Game/Board.h
@@ -1,12 +1,23 @@
 #pragma once
 #ifndef BOARD_H
 #define BOARD_H
+#include <vector>
 
 #include "Hex.h"
 
+//TODO:
+// 
+// : class drewable
+
 class Board {
 public:
+    Board(const int size, const int difficultLevel);
+    void drew();
+
 private:
+    void init();
+    std::vector<Hex> m_board;
+    Hex m_dest;
     
 
 };
